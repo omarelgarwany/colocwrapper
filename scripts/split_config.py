cd8da55aabe9bf97f4e731e550920e9624e5b3ec
Omar El Garwany <oe2@sanger.ac.uk>
2022-11-09 10:40:02 +0000

    use argparse in split_config.py

--- scripts/split_config.py
+++ scripts/split_config.py
@@ -1,9 +1,18 @@
 import numpy as np
 import pandas as pd
-import sys
-conf_f=sys.argv[1]
-num_chunks=sys.argv[2]
-chunkoutput_fs=sys.argv[3]
+import argparse
+
+parser = argparse.ArgumentParser(description='Split configuration file into chunks')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='an integer for the accumulator')
+parser.add_argument('--config-file', dest='conf_f',required=True, help='Configuration file')
+parser.add_argument('--num-chunks', dest='num_chunks',required=True, help='number of chunks')
+parser.add_argument('--chunk-files', dest='chunkoutput_fs',required=True, nargs='+',help='chunk files')
+args = parser.parse_args()
+
+conf_f=args.conf_f
+num_chunks=args.num_chunks
+chunkoutput_fs=args.chunkoutput_fs
 
 conf_dat=pd.read_csv(conf_f,sep='\t',header=None)
 conf_dat_chunked=np.array_split(conf_dat, num_chunks)
